faaff6430c6263f5c0eb6d0e20d1cbddc3194a35
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2018-11-21 18:12:43 +0100

    MGM: Turn Import source code into a Fsctl function

--- mgm/XrdMgmOfs.hh
+++ mgm/XrdMgmOfs.hh
@@ -1781,6 +1781,17 @@ private:
              const XrdSecEntity* client);
 
   //----------------------------------------------------------------------------
+  //! Handle file importation into the namespace operation
+  //----------------------------------------------------------------------------
+  int Import(const char* path,
+             const char* ininfo,
+             XrdOucEnv& env,
+             XrdOucErrInfo& error,
+             eos::common::LogId& ThreadLogId,
+             eos::common::Mapping::VirtualIdentity& vid,
+             const XrdSecEntity* client);
+
+  //----------------------------------------------------------------------------
   //! Query to determine if current node is acting as master
   //----------------------------------------------------------------------------
   int IsMaster(const char* path,
